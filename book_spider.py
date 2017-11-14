a5767ccf742dc2f99b5934a70f5a2adb40aa9162
z550665887 <447143800@qq.com>
2017-11-14 15:48:13 +0800

    添加自动搜索功能，只需要输入书名即可自动爬取同时邮件发送

--- book_spider.py
+++ book_spider.py
@@ -7,10 +7,11 @@ import re
 import threading
 import time
 
+
 path = "D:/小说爬虫"    ###文件路径
-name = "火影之活久见"     ###文件姓名
-url = "http://www.biqu.cm/28_28691/"        ##起始IP 
+book_name = "修真聊天群"     ###文件姓名
 num = 20 ##线程数
+
 header  = {
     "Host" :"www.biqu.cm",
     "User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64; rv:30.0) Gecko/20100101 Firefox/30.0"  
@@ -19,9 +20,21 @@ proxies ={              ###代理IP需要顶似乎更新
     "http":'http://58.16.42.112:80'
 }
 
+def search(book_name):
+    a = book_name.encode("gb2312")
+    data ={
+        "type":"articlename",
+        "s":a,
+        "submit":''
+    }
+    url = "http://m.biqu.cm/s.php"
+    s = requests.post(url=url,data= data,headers=header,proxies = proxies)
+    tree = html.fromstring(s.text)
+    url_back = tree.xpath("//p[@class='line']/a/@href")
+    return url_back[0].replace('/wap','')
 
-def run():
-    global path,name,url,header,proxies
+def run(url):
+    global path,book_name,header,proxies
     s = requests.get(url,headers=header,proxies = proxies)
     tree = html.fromstring(s.text)
     url_date = tree.xpath("//dd/a/@href")
@@ -37,12 +50,12 @@ def run():
         if x % num == 0:
             time.sleep(0.1)
     time.sleep(5)
-    print(merge_txt(path,name))     ###将分散的TXT单章合并
-    sendmail4(path,name)
+    print(merge_txt(path,book_name))     ###将分散的TXT单章合并
+    sendmail4(path,book_name)
     print("已发送到邮箱")
 
 def deal(url,page):
-    global path,name,header,proxies
+    global path,book_name,header,proxies
     s = requests.get(url,headers=header,proxies = proxies)
     tree = etree.HTML(s.text.encode('latin-1').decode('GBK'))
     title = tree.xpath("//h1/text()")
@@ -51,7 +64,7 @@ def deal(url,page):
     for y in range(len(message)):
         message[y] = message[y].replace("\xa0\xa0\xa0\xa0","")
     print("开始写入"+str(page)+"章"+title[0])
-    with open('{0}/{1}/{2}.txt'.format(path,name,str(page)), 'w' ,encoding='utf-8') as f:
+    with open('{0}/{1}/{2}.txt'.format(path,book_name,str(page)), 'w' ,encoding='utf-8') as f:
         f.write(title[0])
         for line in message:
             f.write(line)
@@ -59,6 +72,6 @@ def deal(url,page):
 
 
 
-
 if __name__  == "__main__":
-    run()
+    url = "http://www.biqu.cm"+search(book_name)
+    run(url)
